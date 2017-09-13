9d53ac29559ddfdabf4fdb8fb8ef4193c0f16d8c
sgwarnock <31389161+sgwarnock@users.noreply.github.com>
2017-09-13 17:04:08 -0400

    Add files via upload

---
+++ Assignment-4-master/server/config/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  db: {
+    uri: 'mongodb://sgwarnock:miley14@ds119044.mlab.com:19044/assignment3', //place the URI of your mongo database here.
+  },
+  googleMaps: {
+    key: ''
+  },
+  port: 8080
+};
